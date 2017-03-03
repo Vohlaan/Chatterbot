f8907ba570f1cc6b27f9c98ff59d13d0b95e78ad
sirh3e <marvin.henk.pro@gmail.com>
2017-03-03 20:14:34 +0100

    data

---
+++ Loginsystem/Loginsystem/Data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loginsystem
+{
+    class Data
+    {
+        private static string Username;
+
+        public static string GetUsername() {
+            return Username;
+        }
+
+        public static void SetUsername(string Value) {
+            Username = Value;
+        }
+    }
+}
